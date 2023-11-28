5608db00087f4d150011071496c9b61cf6213257
Julipusik <yuliuaboyko@gmail.com>
2023-11-28 20:16:20 +0200

    hw 3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,21 @@
 import express from "express";
 import contactsController from "../../controllers/contacts/contacts-controller.js";
-import isEmptyBody from "../../middlewares/isEmptyBody.js";
-import { addContactSchema, updateContactSchema } from "../../schemas/contact-schema.js";
+import { isEmptyBody, isValidId } from "../../middlewares/isEmptyBody.js";
+import { addContactSchema, contactFavoriteSchema, updateContactSchema } from "../../models/Contact.js";
 import validateWrapper from "../../decorators/validateWrapper.js";
 
 const router = express.Router();
 
 router.get('/', contactsController.getAllContacts);
 
-router.get('/:contactId', contactsController.getContactById);
+router.get('/:contactId', isValidId, contactsController.getContactById);
 
 router.post('/', isEmptyBody, validateWrapper(addContactSchema), contactsController.addContact);
 
-router.delete('/:contactId', contactsController.deleteContact);
+router.put('/:contactId', isValidId, isEmptyBody, validateWrapper(updateContactSchema), contactsController.updateContact);
 
-router.put('/:contactId', isEmptyBody, validateWrapper(updateContactSchema), contactsController.updateContact);
+router.patch("/:contactId/favorite", isValidId, isEmptyBody, validateWrapper(contactFavoriteSchema), contactsController.getContactById)
+
+router.delete('/:contactId', isValidId, contactsController.deleteContact);
 
 export default router;
