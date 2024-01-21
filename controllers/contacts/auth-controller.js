aa0fa8beedefe9e24a4b223753c55642ab6af334
Julipusik <yuliuaboyko@gmail.com>
2024-01-21 13:31:13 +0200

    hw 6

--- controllers/contacts/auth-controller.js
+++ controllers/contacts/auth-controller.js
@@ -2,16 +2,17 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import "dotenv/config.js";
 import User from "../../models/User.js";
-import { HttpError } from "../../helpers/index.js";
+import { HttpError, sendEmail } from "../../helpers/index.js";
 import tryCatchWrapper from "../../decorators/tryCatchWrapper.js";
 import fs from "fs/promises";
 import path from "path";
 import gravatar from "gravatar";
 import Jimp from "jimp";
+import { nanoid } from "nanoid";
 
 const avatarsPath = path.resolve("public", "avatars");
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, BASE_URL } = process.env;
 
 const signup = async (req, res) => {
     const { email, password } = req.body;
@@ -21,10 +22,18 @@ const signup = async (req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const verificationToken = nanoid();
 
     const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken });
+
+    const verifyEmail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click verify email</a>`
+    };
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
         user: {
@@ -34,17 +43,65 @@ const signup = async (req, res) => {
     });
 }
 
-const signin = async (req, res) => {
+const verify = async (req, res, next) => {
+    try {
+        const { verificationToken } = req.params;
+        const user = await User.findOne({ verificationToken });
+        if (!user) {
+            throw HttpError(404, "User not found");
+        }
+
+        await User.findByIdAndUpdate(user._id, {
+            verify: true,
+            verificationToken: "",
+        });
+
+        res.json({
+            message: "Verification successful",
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const resendVerify = async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw HttpError(400, "Missing required field Email");
+        }
+        if (user.verify) {
+            throw HttpError(400, "Verification has been already passed");
+        }
+        const verifyEmail = {
+            to: email,
+            subject: "Verify email",
+            html: `<a target="_blank" href="${BASE_URL}/>api/users/verify/${user.verificationToken}">Click verify email</a>`
+        };
+
+        await sendEmail(verifyEmail);
+
+        res.json({ message: "Email sent successfully" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const signin = async (req, res, next) => {
     try {
         const { email, password, subscription } = req.body;
         const user = await User.findOne({ email });
         if (!user) {
             throw HttpError(401, "Email or password invalid");
-        }
+        };
+        if (!user.verify) {
+            throw HttpError(401, "Email not verified");
+        };
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
             throw HttpError(401, "Email or password invalid");
-        }
+        };
 
         const payload = {
             id: user._id,
@@ -60,7 +117,7 @@ const signin = async (req, res) => {
             },
         })
     } catch (error) {
-        throw HttpError(401, "Email or password is wrong");
+        next(error);
     }
 }
 
@@ -120,6 +177,8 @@ const updateAvatar = async (req, res) => {
 
 export default {
     signup: tryCatchWrapper(signup),
+    verify: tryCatchWrapper(verify),
+    resendVerify: tryCatchWrapper(resendVerify),
     signin: tryCatchWrapper(signin),
     getCurrent: tryCatchWrapper(getCurrent),
     signout: tryCatchWrapper(signout),
